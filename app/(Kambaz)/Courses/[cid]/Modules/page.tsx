a327042d4969c9f8c7180e9ae42d3c6572af2de3
Charlie Miller <charliemiller1016@gmail.com>
2025-09-19 07:32:03 -0400

    add week descriptions

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -24,9 +24,27 @@ export default function Modules() {
                 </li>
                 <li className="wd-module">
                     <div className="wd-title">Week 2</div>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">LEARNING OBJECTIVES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">CSS/Bootstrap</li>
+                                <li className="wd-content-item">Learn what is CSS</li>
+                            </ul>
+                        </li>
+                    </ul>
                 </li>
                 <li className="wd-module">
                     <div className="wd-title">Week 3</div>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">LEARNING OBJECTIVES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">React/TypeScript</li>
+                                <li className="wd-content-item">Learn what is React</li>
+                            </ul>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
