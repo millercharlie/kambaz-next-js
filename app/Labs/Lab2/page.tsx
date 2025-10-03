ccf8e8959c22aa525af9612c7e86e4bfb46d3913
Charlie Miller <charliemiller1016@gmail.com>
2025-10-03 16:13:06 -0400

    Fix HTML Quotations

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,24 +1,24 @@
-import './index.css';
 import './ForegroundColors';
-import ForegroundColors from '@/app/Labs/Lab2/ForegroundColors';
+import './index.css';
 import BackgroundColors from '@/app/Labs/Lab2/BackgroundColors';
-import Borders from '@/app/Labs/Lab2/Borders';
-import Padding from '@/app/Labs/Lab2/Padding';
-import Margin from '@/app/Labs/Lab2/Margin';
-import Dimensions from '@/app/Labs/Lab2/Dimensions';
-import Positions from '@/app/Labs/Lab2/Positions';
-import ZIndex from '@/app/Labs/Lab2/ZIndex';
-import Float from '@/app/Labs/Lab2/Float';
-import GridLayout from '@/app/Labs/Lab2/GridLayout';
-import Flex from '@/app/Labs/Lab2/Flex';
-import ReactIconsSampler from '@/app/Labs/Lab2/ReactIcons';
-import { Container } from 'react-bootstrap';
-import BootstrapGrids from '@/app/Labs/Lab2/BootstrapGrids';
-import ScreenSizeLabel from '@/app/Labs/Lab2/ScreenSizeLabel';
-import BootstrapTables from '@/app/Labs/Lab2/BootstrapTables';
-import BootstrapLists from '@/app/Labs/Lab2/BootstrapLists';
 import BootstrapForms from '@/app/Labs/Lab2/BootstrapForms';
+import BootstrapGrids from '@/app/Labs/Lab2/BootstrapGrids';
+import BootstrapLists from '@/app/Labs/Lab2/BootstrapLists';
 import BootstrapNavigation from '@/app/Labs/Lab2/BootstrapNavigation';
+import BootstrapTables from '@/app/Labs/Lab2/BootstrapTables';
+import Borders from '@/app/Labs/Lab2/Borders';
+import Dimensions from '@/app/Labs/Lab2/Dimensions';
+import Flex from '@/app/Labs/Lab2/Flex';
+import Float from '@/app/Labs/Lab2/Float';
+import ForegroundColors from '@/app/Labs/Lab2/ForegroundColors';
+import GridLayout from '@/app/Labs/Lab2/GridLayout';
+import Margin from '@/app/Labs/Lab2/Margin';
+import Padding from '@/app/Labs/Lab2/Padding';
+import Positions from '@/app/Labs/Lab2/Positions';
+import ReactIconsSampler from '@/app/Labs/Lab2/ReactIcons';
+import ScreenSizeLabel from '@/app/Labs/Lab2/ScreenSizeLabel';
+import ZIndex from '@/app/Labs/Lab2/ZIndex';
+import { Container } from 'react-bootstrap';
 
 export default function Lab2() {
   return (
@@ -27,7 +27,7 @@ export default function Lab2() {
       <h3>Styling with the STYLE attribute</h3>
       <p>
         Style attribute allows configuring look and feel right on the element.
-        Although it's very convenient it is considered bad practice and you
+        Although it&quot;s very convenient it is considered bad practice and you
         should avoid using the style attribute
       </p>
       <div id='wd-css-id-selectors'>
@@ -37,15 +37,15 @@ export default function Lab2() {
           name, e.g., P, we can refer to a specific element by its ID
         </p>
         <p id='wd-id-selector-2'>
-          Here's another paragraph using a different ID and a different look and
-          feel
+          Here&quot;s another paragraph using a different ID and a different
+          look and feel
         </p>
       </div>
       <div id='wd-css-class-selectors'>
         <h3>Class selectors</h3>
         <p className='wd-class-selector'>
-          Instead of using IDs to refer to elements, you can use an element's
-          CLASS attribute
+          Instead of using IDs to refer to elements, you can use an
+          element&quot;s CLASS attribute
         </p>
         <h4 className='wd-class-selector'>
           This heading has same style as paragraph above
@@ -58,7 +58,7 @@ export default function Lab2() {
             Selectors can be combined to refer elements in particular places in
             the document
             <p className='wd-selector-3'>
-              This paragraph's red background is referenced as
+              This paragraph&quot;s red background is referenced as
               <br />
               .selector-2 .selector3
               <br />
