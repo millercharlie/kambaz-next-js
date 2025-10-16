a4629fa1d05ee0febc4db7dcad892e4277df1914
Charlie Miller <charliethegreat@One-MacBook-Pro.local>
2025-10-16 17:19:24 -0400

    fix linting 3

--- app/Labs/Lab3/BooleanVariables.tsx
+++ app/Labs/Lab3/BooleanVariables.tsx
@@ -7,7 +7,7 @@ const BooleanVariables = () => {
   const true2 = true1 || false1;
   const true3 = !false2;
   const true4 = numberVariable === 123; // always use === not ==
-  // @ts-expect-error
+  // @ts-expect-error this fails the production
   const true5 = floatingPointNumber !== 321.432;
   const false3 = numberVariable < 100;
   return (
